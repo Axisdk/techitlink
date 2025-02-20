b131eac0c04116b1cef69bc0712cf4c57b753805
Danil <timspire10082019@gmail.com>
2025-02-20 23:48:56 +0500

    add: auth module, sign-in page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {AuthComponent} from "./modules/auth/auth.component";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AuthComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/modules/auth/auth.component.ts
@@ -0,0 +1,13 @@
+import {Component} from "@angular/core";
+
+@Component({
+  selector: 'app-auth',
+  templateUrl: './auth.component.html',
+  standalone: true
+})
+
+export class AuthComponent {
+
+  constructor() {}
+
+}
